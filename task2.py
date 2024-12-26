09e0528a6106bff2a5b274e6888e13a35a10fcd7
Promit Purkayastha <86530357+Promitpro@users.noreply.github.com>
2024-12-27 01:27:43 +0600

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+
+image_path = 'E:\\OneDrive\\Documents\\4th year 1st semester\\digital image processing\\images\\DIP_Lab_task_02.jpg'  # Replace with your image path
+image = cv2.imread(image_path)
+
+height, width, channels = image.shape
+gray_image = np.zeros((height, width), dtype=np.uint8)
+
+for y in range(height):
+    for x in range(width):
+        R = image[y, x, 2] 
+        G = image[y, x, 1]
+        B = image[y, x, 0]
+        gray_value = int(0.299 * R + 0.587 * G + 0.114 * B)
+        gray_image[y, x] = gray_value
+
+top_left_x = 50  
+top_left_y = 50  
+sub_region_size = 5 
+
+sub_region = gray_image[top_left_y:top_left_y + sub_region_size, 
+                        top_left_x:top_left_x + sub_region_size]
+
+print("Sub-region intensity values (5x5):")
+print(sub_region)
+
+cv2.imshow('Original RGB Image', image)
+cv2.imshow('Grayscale Image', gray_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
